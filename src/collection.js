b23ee420882ac7d867aed009c472e4dfa2fd19dc
Ismail Ceylan <ismaillceylan@gmail.com>
2024-01-15 14:44:33 +0300

    refactor: iterable interface

--- src/collection.js
+++ src/collection.js
@@ -13,4 +13,42 @@ export default class Collection
 	{
 		return this.#data[ 0 ];
 	}
+
+	get( index )
+	{
+		return this.#data[ index ];
+	}
+
+	[ Symbol.iterator ]()
+	{
+		let index = 0;
+
+		return {
+			next: () =>
+			{
+				if( index < this.#data.length )
+				{
+					return {
+						value: this.#data[ index++ ],
+						done: false
+					}
+				}
+				else
+				{
+					return { done: true }
+				}
+			}
+		}
+	}
 }
+
+Object.setPrototypeOf( Collection.prototype, new Proxy( Collection.prototype,
+{
+	get( _target, key, instance )
+	{
+		if( isNaN( key ) == false )
+		{
+			return instance.get( key );
+		}
+	}
+}));
